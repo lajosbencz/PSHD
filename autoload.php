6c8b1dba712108ff99be86dcb44b44ae5c285ee7
lazos <lajos.bencz@gmail.com>
2015-03-02 13:21:40 +0100

    Added autoload file

---
+++ autoload.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__.'/src/Exception.php';
+require_once __DIR__.'/src/Literal.php';
+require_once __DIR__.'/src/Model.php';
+require_once __DIR__.'/src/Where.php';
+require_once __DIR__.'/src/Statement.php';
+require_once __DIR__.'/src/Result.php';
+require_once __DIR__.'/src/Select.php';
+require_once __DIR__.'/src/PSHD.php';
